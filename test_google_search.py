7db24fc7442b029d6f6532dbc421efaf4a1787b5
Azamat Abdurashidov <royalty5611@gmail.com>
2024-06-24 01:04:24 +0300

    add conftest file, add fixture

--- test_google_search.py
+++ test_google_search.py
@@ -1,5 +1,6 @@
 from selene import browser, be, have
 
+
 def test_positive_search_google(open_browser):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
