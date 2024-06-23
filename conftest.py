2dd89b9b2f1242fecab786fe7a0812c6de5ea757
Azamat Abdurashidov <royalty5611@gmail.com>
2024-06-24 01:23:22 +0300

    add conftest file, add fixture

--- conftest.py
+++ conftest.py
@@ -1,9 +1,9 @@
 import pytest
 from selene import browser
 
-@pytest.fixture()
+@pytest.fixture(autouse=True)
 def open_browser():
-    browser.open('https://google.com')
+    browser.config.base_url = 'https://google.com'
     browser.config.window_height = 1920
     browser.config.window_width = 1080
     yield
--- test_google_search.py
+++ test_google_search.py
@@ -1,12 +1,14 @@
 from selene import browser, be, have
 
 
-def test_positive_search_google(open_browser):
+def test_positive_search_google():
+    browser.open('')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
-def test_negative_search_google(open_browser):
+def test_negative_search_google():
+    browser.open('')
     text = 'jkbdsfkjsbd jksbdfkjsbd. jksbdfjkbsdfkjbsdf'
     browser.element('[name="q"]').should(be.blank).type(text).press_enter()
     browser.element('[class="card-section"]').should(have.text(f'По запросу {text} ничего не найдено.'))
